dec97cc04432f71da583c4e9497112d936dee688
SalehBinHamed <157858533+SalehBinHamed@users.noreply.github.com>
2024-10-03 16:58:29 +0300

    done

---
+++ Bouns.py
@@ -0,0 +1,7 @@
+n,number,sum =int(input("inter a number")) , 1 ,0
+while number <= n:
+    if number % 2 == 0:
+        sum += number
+        print(sum)
+    number += 1
+print(f"The sum of even numbers between {number} and {n} is {sum}.")
